dda59c51d8b578c5a7876ec804caa01935e47f29
Kilarin <gith-dch00@sneakemail.com>
2019-05-26 22:08:59 -0500

    Massive update, many changes, close to ready for release

--- realms_map_generators/bg_basic_biomes.lua
+++ realms_map_generators/bg_basic_biomes.lua
@@ -1,10 +1,12 @@
 --this was inspired by and takes some code from
 --https://github.com/SmallJoker/noisetest WTFPL License
 
+bg_basic_biomes={}
 
 local c_air = minetest.get_content_id("air")
 local c_stone = minetest.get_content_id("default:stone")
 local c_dirt = minetest.get_content_id("default:dirt")
+local c_sand = minetest.get_content_id("default:sand")
 
 --local c_snow = minetest.get_content_id("default:snow")                                   --arctic (no trees)
 local c_ice = minetest.get_content_id("default:ice")                                     --arctic (no trees)
@@ -48,7 +50,7 @@ local nvals_heat
 
 
 
-function gen_appletree(x, y, z, area, data)
+function bg_basic_biomes.gen_appletree(x, y, z, area, data)
 	for j = -1, 6 do
 		if j == 5 or j == 6 then
 			for i = -3, 3 do
@@ -78,7 +80,7 @@ function gen_appletree(x, y, z, area, data)
 	end
 end
 
-function gen_jungletree(x, y, z, area, data)
+function bg_basic_biomes.gen_jungletree(x, y, z, area, data)
 	for j = -1, 14 do
 		if j == 8 or j == 10 or j == 14 then
 			for i = -3, 3 do
@@ -96,7 +98,7 @@ function gen_jungletree(x, y, z, area, data)
 end
 
 
-function place_material(x,y,z, area, data, material)
+function bg_basic_biomes.place_material(x,y,z, area, data, material)
 	local vi = area:index(x, y, z)
 	data[vi] = material
 end --place mateiral
@@ -104,21 +106,21 @@ end --place mateiral
 --  m
 -- mxm   x=location xyz which is NOT modified
 --  m
-function place_compass(x,y,z, area,data, material, distance)
-	place_material(x+distance,y,z, area,data, material)
-	place_material(x-distance,y,z, area,data, material)
-	place_material(x,y,z+distance, area,data, material)
-	place_material(x,y,z-distance, area,data, material)
-end--place_compass
+function bg_basic_biomes.place_compass(x,y,z, area,data, material, distance)
+	bg_basic_biomes.place_material(x+distance,y,z, area,data, material)
+	bg_basic_biomes.place_material(x-distance,y,z, area,data, material)
+	bg_basic_biomes.place_material(x,y,z+distance, area,data, material)
+	bg_basic_biomes.place_material(x,y,z-distance, area,data, material)
+end--bg_basic_biomes.place_compass
 
 -- mmm
 -- mxm x=location xyz which is NOT modified
 -- mmm
-function place_surround(x,y,z, area, data, material)
+function bg_basic_biomes.place_surround(x,y,z, area, data, material)
 	for j=-1,1 do
 		for k=-1,1 do
 			if j~=0 or k~=0 then
-				place_material(x+j,y,z+k, area,data, material)
+				bg_basic_biomes.place_material(x+j,y,z+k, area,data, material)
 			end --if j
 		end --for k
 	end --for j
@@ -126,63 +128,68 @@ end--place_four_around
 
 
 
-function gen_pinetree(x, y, z, area, data)
+function bg_basic_biomes.gen_pinetree(x, y, z, area, data)
 	local vi
 	for j = -1, 6 do
-		place_material(x,y+j,z, area,data, c_ptree)
+		bg_basic_biomes.place_material(x,y+j,z, area,data, c_ptree)
 		if j==2 or j==4 or j==6 then 
-			place_compass(x,y+j,z, area,data, c_pleaves, 1)
+			bg_basic_biomes.place_compass(x,y+j,z, area,data, c_pleaves, 1)
 		end --if j 2 4 or 6
 		if j==3 then
-			place_surround(x,y+j,z, area,data, c_pleaves)
+			bg_basic_biomes.place_surround(x,y+j,z, area,data, c_pleaves)
 			for i=-1,1 do 
-				place_material(x+i,y+j,z-2, area,data, c_pleaves)
-				place_material(x+i,y+j,z+2, area,data, c_pleaves)
+				bg_basic_biomes.place_material(x+i,y+j,z-2, area,data, c_pleaves)
+				bg_basic_biomes.place_material(x+i,y+j,z+2, area,data, c_pleaves)
 			end --for i
 			for k=-1,1 do
-				place_material(x+2,y+j,z+k, area,data, c_pleaves)
-				place_material(x-2,y+j,z+k, area,data, c_pleaves)
+				bg_basic_biomes.place_material(x+2,y+j,z+k, area,data, c_pleaves)
+				bg_basic_biomes.place_material(x-2,y+j,z+k, area,data, c_pleaves)
 			end --for k
-		place_compass(x,y+j,z, area,data, c_pleaves, 3)
+		bg_basic_biomes.place_compass(x,y+j,z, area,data, c_pleaves, 3)
 		end --if j=3
 		if j==5 then
-			place_surround(x,y+j,z, area,data, c_pleaves) 
-			place_compass(x,y+j,z, area,data, c_pleaves, 2)
+			bg_basic_biomes.place_surround(x,y+j,z, area,data, c_pleaves) 
+			bg_basic_biomes.place_compass(x,y+j,z, area,data, c_pleaves, 2)
 		end --if j==5
 	end--for j
-	place_material(x,y+7,z, area,data, c_pleaves)
-end--gen_pinetree
+	bg_basic_biomes.place_material(x,y+7,z, area,data, c_pleaves)
+end--bg_basic_biomes.gen_pinetree
 
 
 local biome={}
 biome[biomes.ARCTIC]={}
-biome[biomes.ARCTIC].under=c_ice
+biome[biomes.ARCTIC].node_top=c_ice
+biome[biomes.ARCTIC].node_filler=c_ice
 biome[biomes.ARCTIC].tree=nil
 biome[biomes.ARCTIC].treechance=0  --treechance is in a thousand
 
 biome[biomes.COLD]={}
-biome[biomes.COLD].under=c_dirt_snow
-biome[biomes.COLD].tree=gen_pinetree
+biome[biomes.COLD].node_top=c_dirt_snow
+biome[biomes.COLD].node_filler=c_dirt
+biome[biomes.COLD].tree=bg_basic_biomes.gen_pinetree
 biome[biomes.COLD].treechance=15
 
 biome[biomes.WARM]={}
-biome[biomes.WARM].under=c_grass
-biome[biomes.WARM].tree=gen_appletree
+biome[biomes.WARM].node_top=c_grass
+biome[biomes.WARM].node_filler=c_dirt
+biome[biomes.WARM].tree=bg_basic_biomes.gen_appletree
 biome[biomes.WARM].treechance=5
 
 biome[biomes.HOT]={}
-biome[biomes.HOT].under=c_dirt_rainforest
-biome[biomes.HOT].tree=gen_jungletree
+biome[biomes.HOT].node_top=c_dirt_rainforest
+biome[biomes.HOT].node_filler=c_dirt
+biome[biomes.HOT].tree=bg_basic_biomes.gen_jungletree
 biome[biomes.HOT].treechance=50
 
 biome[biomes.DESERT]={}
-biome[biomes.DESERT].under=c_desert_sand
+biome[biomes.DESERT].node_top=c_desert_sand
+biome[biomes.DESERT].node_filler=c_desert_sand
 biome[biomes.DESERT].tree=nil
 biome[biomes.DESERT].treechance=0
 
 
 --********************************
-function get_biome(nixz)
+function bg_basic_biomes.get_biome(nixz)
 	local n_biome = nvals_heat[nixz] * 10
 	local bi
 	if n_biome < heat_params.arctic then
@@ -196,9 +203,9 @@ function get_biome(nixz)
 	else
 		bi = biomes.DESERT
 	end --if
-	--minetest.log("get_biome-> nixz="..nixz.." n_biome="..n_biome.." bi="..bi)
+	--minetest.log("bg_basic_biomes.get_biome-> nixz="..nixz.." n_biome="..n_biome.." bi="..bi)
 	return bi
-end --get_biome
+end --bg_basic_biomes.get_biome
 
 
 --********************************
@@ -209,31 +216,46 @@ function gen_bg_basic_biomes(parms)
 		local t1 = os.clock()
 		--minetest.log("gen_bg_basic_biomes top isect minp="..luautils.pos_to_str(parms.isect_minp).." maxp="..luautils.pos_to_str(parms.isect_maxp))
 	
-		--get noise details
-		local isectsize = luautils.box_sizexz(parms.isect_minp,parms.isect_maxp)
-		local minposxz = {x=parms.isect_minp.x, y=parms.isect_minp.z}
-	
-		nvals_heat = minetest.get_perlin_map(np_heat, isectsize):get_2d_map_flat(minposxz)
+		nvals_heat = minetest.get_perlin_map(np_heat, parms.isectsize2d):get_2d_map_flat(parms.minposxz)
 	
 		local nixz=1
 		for z=parms.isect_minp.z, parms.isect_maxp.z do
 			for x=parms.isect_minp.x, parms.isect_maxp.x do
-				local bi=get_biome(nixz)
-				local top=parms.share.surface[z][x]
-				local vi = parms.area:index(x, top, z) -- This accesses the node at a given position
-				parms.data[vi]=biome[bi].under
-				if math.random(1000)<=biome[bi].treechance then biome[bi].tree(x,top+1,z,parms.area,parms.data) end 
+				--SOME of the surface should be in this chunk, but not necesarilly all of it
+				--we have to check for it because we can end up with strange floating surfaces
+				--if we try to update a top that isn't in this chunk
+				local srfc=parms.share.surface[z][x].top
+				if srfc<=parms.isect_maxp.y and srfc>=parms.isect_minp.y --srfc is in this chunk
+				    and srfc>=parms.sealevel then --don't mess with underwater (for this biome generator)
+					local bi=bg_basic_biomes.get_biome(nixz)
+					local vi = parms.area:index(x, srfc, z) -- This accesses the node at a given position
+					parms.data[vi]=biome[bi].node_top
+					if math.random(1000)<=biome[bi].treechance then biome[bi].tree(x,srfc+1,z,parms.area,parms.data) end 
+				end --if srfc
+				--now see if we need to convert the underneath stuff
+				if (srfc-1)>=parms.isect_minp.y then --this wont work right, don't have dirtbot yet
+					local j=-1
+					local vi = parms.area:index(x, srfc+j, z) -- This accesses the node at a given position
+					local bi=bg_basic_biomes.get_biome(nixz)
+					--minetest.log("gen_bg_basic_biomes-> bi="..bi.." j="..j.." parms.data[vi]="..parms.data[vi].." c_dirt="..c_dirt)
+					while parms.data[vi]==c_dirt do  --got to check for end of chunk as well
+						local bi=bg_basic_biomes.get_biome(nixz)
+						parms.data[vi]=biome[bi].node_filler
+						j=j-1
+						vi=parms.area:index(x, srfc+j, z) 
+						--minetest.log("gen_bg_basic_biomes-> bi="..bi.." j="..j.." parms.data[vi]="..parms.data[vi].." c_dirt="..c_dirt)
+					end --while
+				end --if
 				nixz=nixz+1
 			end --for x
 		end --for z
-	--store dirttop in parms.share so it can be passed to a decoration/biome generator
-	parms.share.surface=dirttop
+
 	
 		local chugent = math.ceil((os.clock() - t1) * 1000) --grab how long it took
 		minetest.log("gen_bg_basic_biomes-> END isect="..luautils.pos_to_str(parms.isect_minp).." - "..luautils.pos_to_str(parms.isect_maxp).."  "..chugent.." ms") --tell people how long
 	end --if parms.share.surface
 end -- gen_very_simple
 	
-realms.register_rmg("bg_basic_biomes",gen_bg_basic_biomes)
+realms.register_mapgen("bg_basic_biomes",gen_bg_basic_biomes)
 
 
